64b78f9337586feb170ce89cc81877da7171f37d
SrAlmeja <cdmx1895@amerike.edu.mx>
2023-02-20 18:02:31 -0600

    Se crea una pantalla blanca

--- EL Vacio/EL Vacio/Main.cpp
+++ EL Vacio/EL Vacio/Main.cpp
@@ -6,6 +6,27 @@
 int main()
 {
     glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
+    
+    //Apuntador de tipo window para tener referencia de la ventana (Referencia)
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Window", NULL, NULL);
+
+    //Crear un contexto para openGL
+    glfwMakeContextCurrent(window);
+
+    //Mantener la viva la ventana hasta que se cierre
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+    
+    //DESTRUIR VENTANA
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
     return 0;
 }
