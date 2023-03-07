df808c906f1daece0a47e4101dfa45356489eaa8
SrAlmeja <cdmx1895@amerike.edu.mx>
2023-03-06 18:28:33 -0600

    Hasta aquí ya lo arregle

--- Nuevo Vacio/Nuevo Vacio/Main.cpp
+++ Nuevo Vacio/Nuevo Vacio/Main.cpp
@@ -18,12 +18,12 @@ int main()
 
     GLfloat vertices[] =
     {
-         -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,       // Esquina inferior izq
-         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,        // Esquina inferior derecha
-         0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   // Esquina superior
-         -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,      // Interior izquierda
-         0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,    // Interior derecha
-         0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f        // Interior abajo
+         -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,       0.8f, 0.3f , 0.02f, // Esquina inferior izq
+         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,        0.8f, 0.3f , 0.02f, // Esquina inferior derecha
+         0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f , 0.32f, // Esquina superior
+         -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,    0.9f, 0.45f, 0.17f, // Interior izquierda
+         0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Interior derecha
+         0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,         0.9f, 0.3f , 0.02f// Interior abajo
     };
 
     GLfloat insideVertex[] =
@@ -75,11 +75,16 @@ int main()
     VBO VBO1(vertices, sizeof(vertices));
     EBO EBO1(indices, sizeof(indices));
 
-    VAO1.LinkVBO(VBO1, 0);
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
 
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
+
+    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
 
     glViewport(0, 0, 800, 800);
     glfwSwapBuffers(window);
@@ -88,10 +93,11 @@ int main()
     //Asignacion de buffer
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(1, 0.5f, 0, 0);
+        glClearColor(0.0f, 0.0f, 0.0f, 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
         shaderProgram.Activate();
+        glUniform1f(uniID, 0.5f);
         VAO1.Bind();
         glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
--- Nuevo Vacio/Nuevo Vacio/default.frag
+++ Nuevo Vacio/Nuevo Vacio/default.frag
@@ -1,6 +1,8 @@
-version 330 core
+#version 330 core
 out vec4 FragColor;
+
+
 void main()
 {
-    FragColor = vec4(1, 0, 1, 0.5);
+    FragColor = vec4(1.0f, 0.0f, 0.0f, 1);
 }
